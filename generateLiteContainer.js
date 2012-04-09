d066a3c190d9190742efbb3a97eef33a2f106e64
Patrick Rauscher <prauscher@ohai.su>
2012-04-09 00:34:51 +0000

    blah

--- generateLiteContainer.js
+++ generateLiteContainer.js
@@ -1,9 +1,19 @@
 function generateLiteContainer() {
-	var link = $.post("http://collabeverywhere.net/publicapi/0/createPad", { "text" : $(rightclicked_item).text() }, function (data) {
+	var link = $.post("http://collabeverywhere.net/publicapi/0/createPad", { "text" : $(rightclicked_item).val() }, function (data) {
 		var frame = $("<iframe>")	.attr("src", data.data.host + data.data.baseUrl + data.data.padID)
 						.css("border", "0px")
-//						.attr("height", $(rightclicked_item).attr("height"))
-//						.attr("width", $(rightclicked_item).attr("width"));
-		$(rightclicked_item).after(frame);
+						.attr("height", $(rightclicked_item).height())
+						.attr("width", $(rightclicked_item).width());
+		var button = $("<button>").text("Rev").click(function () {
+			$.get(data.data.host + data.data.baseUrl + data.data.padID + "/export/txt", function (text) {
+				alert(text);
+				$(this).prev().val(text);
+			});
+			$(this).prev().removeClass("hiddenTextbox");
+			$(this).next().detach();
+			$(this).detach();
+		});
+
+		$(rightclicked_item).addClass("hiddenTextbox").after(frame).after(button);
 	}, "json");
 }
